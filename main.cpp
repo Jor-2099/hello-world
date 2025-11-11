aa13ec588779b1e5f4180b71a9ae7227b3932b55
Jor-2099 <a01805057@tec.mx>
2025-11-11 06:35:12 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+#include <iostream>
+using namespace std;
+
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    int n1 = 50;
+    int n2 = 20;
+    int suma = n1 + n2;
+    cout << "n1 es: " << n1 << endl;
+    cout << "n2 es: " << n2 << endl;
+    cout << "La suma de ambas sera: " << suma << endl;
+
+    return 0;// TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+}
+    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
